39466e1ee8a7be6ce02b4c71252c7bd60361ff5d
paneexiong970-maker <paneexiong970@gmail.com>
2025-11-23 12:48:38 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
-void main() => runApp(MaterialApp(
-  home: Home(),
-));
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+import 'package:world_time/pages/choose_location.dart';
 
-  
-  
+void main() => runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
+));
